f64cc16f0610506164cdb8388f1727a559ae4ea8
Marianne Mateo <mariannejmb18@gmail.com>
2022-11-30 21:25:03 -0400

    Add project files.

---
+++ SistemaVenta.Entity/Rol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Entity;
+
+public partial class Rol
+{
+    public int IdRol { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public bool? EsActivo { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public virtual ICollection<RolMenu> RolMenus { get; } = new List<RolMenu>();
+
+    public virtual ICollection<Usuario> Usuarios { get; } = new List<Usuario>();
+}
---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}

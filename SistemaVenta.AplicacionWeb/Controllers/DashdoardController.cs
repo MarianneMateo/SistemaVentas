6fe5d44f67ace8ec8ab4d56929b6e8165a3154e8
Marianne Mateo <mariannejmb18@gmail.com>
2022-12-03 10:22:18 -0400

    Plantilla Dashboard y Usuarios Lista

---
+++ SistemaVenta.AplicacionWeb/Controllers/DashdoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class DashdoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaVenta.AplicacionWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
